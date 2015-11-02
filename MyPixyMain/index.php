71ed7004085f76c034def2bddd364384d89bf20b
armbrusterchiara <armbruster.chiara@t-online.de>
2015-11-02 11:00:31 +0100

    php - files für Startpage

--- MyPixyMain/index.php
+++ MyPixyMain/index.php
@@ -4,6 +4,8 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+<!-- Das ist die Willkommensseite von MyPixy.
+     Der Visitor/User ist zu diesem Zeitpunkt nicht eingeloggt.-->
 <html>
     <head>
         <meta charset="UTF-8">
